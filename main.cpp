047a049b9f44a7b1b45c32d700cd72fabdde7afe
SabryJkadi <Sabry_kadi@hotmail.com>
2018-11-18 18:18:38 +0100

    A1 done

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
-
+#include <string>
+using namespace std;
 /**
  * The program reads numbers from a file and prints the sum of them
  * Example: main.exe ./tests/input.in
@@ -10,31 +11,77 @@
  */
 int main(int argc, const char * argv[])
 {
-    //fisofjdsifjdsiofjsdifjsdiofjdsifjdsiofjidsjfidsfojdsfoidsjfiodsjfiodsfjdsiofjsdofjsdio
-    //dfhdfgfdgdfggdfgdfgdfgd
+    string input;
+  
+    double *Nums=new double[10];
+    double Readnumbers=0;
+    
+    int Count=0;
+    double sum = 0;
 
-
-
-
-    //jiafjdsifiodsfisdofiosfiodsioefidsojiosfjdsiofdsiofjdsiofjdsiofsjifdjsiofjsd
-    int a = 0;
-    int sum = 0;
+    
+    
     
     std::ifstream myReadFile;
-    
     myReadFile.open(argv[1]);
-    
-    while (myReadFile >> a)
-    {
-        sum += a;
-    }
-    myReadFile.close();
-    
+
+
+
+    while(myReadFile>>Readnumbers){
+
+Readnumbers=(int)(Readnumbers*1000);
+Readnumbers/=1000;  //deciamal precision.
+
+
+
+Nums[Count]=Readnumbers;
+Count++;
+}
     
 
-    
-    std::cout << sum << "\n";
-    
-    return 0;
+
+
+int *copy=new int[Count];
+
+for(int a=0;a<Count;a++){
+
+    copy[a]=Nums[a];
+    sum=sum+copy[a];
+
 }
+
+
+delete [] Nums;
+Nums=nullptr;
+
+cout<<sum<<endl;
+
+for(int a=0; a<Count; a++)
+cout<<copy[a]<<endl;
+
+
+
+cout<<endl;
+cout<<endl;
+
+
+
+
+double avg=sum/Count;
+
+cout<<avg<<"  This is avg"<<endl;
+for(int a=0;a<Count;a++){
+
+    if(copy[a]>avg)
+cout<<"bigger then avg :  "<<copy[a]<<endl;
+}
+
+
+delete[]copy;
+
+
+
+return 0;
+}
+   
 
